07d2bc5796eb403b1186f2b2d6be3089aa642ae9
Mike Norton <nortonmd@icloud.com>
2017-09-20 09:23:45 -0700

    Enable dynamic content
    Remove the static values from the optionlist
    Use the inputSelect UI component to assign values
    Update the js controller to set the value in the component

--- src/aura/HelloWorld/HelloWorldController.js
+++ src/aura/HelloWorld/HelloWorldController.js
@@ -1,5 +1,9 @@
 ({
     myAction : function(component, event, helper) {
 
+    },
+    onSingleSelectChange: function(cmp) {
+        var selectCmp = cmp.find( "InputSelectSingle");
+        cmp.set( "v.whom", selectCmp.get( "v.value"));
     }
 })
